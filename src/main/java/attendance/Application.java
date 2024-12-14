4158ff1a54ca6556df8a2b59c4e48aaff73fc88c
goohong <goohong.chung@gmail.com>
2024-12-14 13:20:04 +0900

    feat(Application): 기본 MVC 구조 생성

--- src/main/java/attendance/Application.java
+++ src/main/java/attendance/Application.java
@@ -1,7 +1,24 @@
 package attendance;
 
+import attendance.controller.AttendanceController;
+import attendance.view.InputView;
+import attendance.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new Application().run();
+    }
+
+    private void run() {
+        AttendanceController attendanceController = initializeComponents();
+        attendanceController.run();
+    }
+
+    private AttendanceController initializeComponents() {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        return new AttendanceController(inputView, outputView);
     }
 }
